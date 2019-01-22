910dd9e505aee16beeddb7e7ab83ae6e77680317
gianlucalimbi <gianluca.limbi@gmail.com>
2019-01-22 19:06:55 +0100

    clean up ViewModelFactory

--- blueprint/src/main/java/com/gianlucalimbi/blueprint/viewmodel/ViewModelFactory.kt
+++ blueprint/src/main/java/com/gianlucalimbi/blueprint/viewmodel/ViewModelFactory.kt
@@ -8,31 +8,12 @@ import javax.inject.Singleton
 
 @Singleton
 class ViewModelFactory @Inject constructor(
-    private val creators: Map<Class<out ViewModel>, Provider<ViewModel>>
+    private val creators: MutableMap<Class<out ViewModel>, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
 
   @Suppress("UNCHECKED_CAST")
   override fun <T : ViewModel> create(viewModelClass: Class<T>): T {
-    var creator: Provider<out ViewModel>? = creators[viewModelClass]
-
-    if (creator == null) {
-      for (creatorClass in creators.keys) {
-        if (viewModelClass.isAssignableFrom(creatorClass)) {
-          creator = creators[creatorClass]
-          break
-        }
-      }
-    }
-
-    if (creator == null) {
-      throw IllegalArgumentException("unknown model class $viewModelClass")
-    }
-
-    try {
-      return creator.get() as T
-    } catch (e: Exception) {
-      throw RuntimeException(e)
-    }
+    return creators[viewModelClass]?.get() as T
   }
 
 }
